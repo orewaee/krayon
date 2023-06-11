aba6daa39af3f26ae8413ebede020533eb55a5c4
orewaee <71517526+orewaee@users.noreply.github.com>
2023-06-11 23:59:05 +0400

    Base

--- src/main/kotlin/ru/orewaee/krayon/Krayon.kt
+++ src/main/kotlin/ru/orewaee/krayon/Krayon.kt
@@ -12,52 +12,18 @@ const val REVERSE = "$ESCAPE[7m"
 const val HIDE = "$ESCAPE[8m"
 
 object Krayon {
-    fun code(
-        color: Color,
-        type: Type = Type.FG
-    ) = "$ESCAPE[${color.code + type.difference}m"
+    private fun generate(body: String) = "$ESCAPE[${body}m"
 
-    fun code(
-        rgb: RGB,
-        type: Type = Type.FG
-    ): String {
-        val (r, g, b) = rgb
-
-        return "$ESCAPE[${38 + type.difference};2;$r;$g;${b}m"
-    }
-
-    fun code(
-        hex: String,
-        type: Type = Type.FG
-    ): String {
-        val r = hex.slice(0..1).toInt(16)
-        val g = hex.slice(2..3).toInt(16)
-        val b = hex.slice(4..5).toInt(16)
-
-        val rgb = RGB(r, g, b)
-
-        return code(rgb, type)
-    }
-
+    /** Generates a color code from a standard set */
     fun color(
-        string: String,
         color: Color,
-        type: Type = Type.FG
-    ) = code(color, type) + "$string$RESET"
+        type: Type = Type.FOREGROUND
+    ) = generate("${color.code + type.difference}")
 
+    /** Generates an RGB color code */
     fun rgb(
-        string: String,
-        rgb: RGB,
-        type: Type = Type.FG
-    ) = code(rgb, type) + "$string$RESET"
-
-    fun hex(
-        string: String,
-        hex: String,
-        type: Type = Type.FG
-    ) = code(hex, type) + "$string$RESET"
+        red: Int, green: Int, blue: Int,
+        type: Type = Type.FOREGROUND
+    ) = generate("${38 + type.difference};2;$red;$green;$blue")
 }
 
-fun String.color(color: Color, type: Type = Type.FG) = Krayon.color(this, color, type)
-fun String.rgb(rgb: RGB, type: Type = Type.FG) = Krayon.rgb(this, rgb, type)
-fun String.hex(hex: String, type: Type = Type.FG) = Krayon.hex(this, hex, type)
--- src/main/kotlin/ru/orewaee/krayon/RGB.kt
+++
@@ -1,7 +0,0 @@
-package ru.orewaee.krayon
-
-data class RGB(
-    val r: Int,
-    val g: Int,
-    val b: Int
-)
