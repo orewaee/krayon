cfab96373790dccb82f57dd9323e5a0dd3334893
orewaee <71517526+orewaee@users.noreply.github.com>
2023-06-12 00:59:44 +0400

    Docs added

--- src/main/kotlin/ru/orewaee/krayon/Krayon.kt
+++ src/main/kotlin/ru/orewaee/krayon/Krayon.kt
@@ -6,25 +6,32 @@ const val RESET = "$ESCAPE[0m"
 object Krayon {
     private fun generate(body: String) = "$ESCAPE[${body}m"
 
-    /** Generates a color code from a standard set */
+    /**
+     * @return standard color code
+     * */
     fun color(
         color: Color,
         type: Type = Type.FOREGROUND
     ) = generate("${color.code + type.difference}")
 
-    /** Generates an RGB color code */
+    /**
+     * @return RGB color code
+     * */
     fun rgb(
         red: Int, green: Int, blue: Int,
         type: Type = Type.FOREGROUND
     ) = generate("${38 + type.difference};2;$red;$green;$blue")
 
+    /**
+     * @return hex color code
+     * */
     fun hex(
         hex: String,
         type: Type = Type.FOREGROUND
     ): String {
         val pattern = Regex("#[0-9A-Fa-f]{6}")
 
-        if (!(hex matches pattern)) return RESET
+        if (!hex.matches(pattern)) return RESET
 
         return rgb(
             hex.slice(1..2).toInt(16),
@@ -34,6 +41,9 @@ object Krayon {
         )
     }
 
+    /**
+     * @return formatting code
+     * */
     fun format(
         vararg format: Format
     ) = format.joinToString("") { generate("${it.code}") }
